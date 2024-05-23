dfed0b1bfb5cc0b30e007e1a66429d15d3511ee4
Adham-Eid27 <adhame900@gmail.com>
2024-05-23 20:15:30 +0300

    update

--- Weather_station/Weather_Station/Weather_Station.ino
+++ Weather_station/Weather_Station/Weather_Station.ino
@@ -1,6 +1,11 @@
 
 int dhtpin = 3;
 
+float rpm = 0;
+unsigned long millisBefore;
+volatile int holes;
+
+
 #include "DHT.h"
 #include <MQ2.h>
 
@@ -8,7 +13,8 @@ float temp;
 float humid;
 
 int MQ2_analog = A0;
-int lpg, co, smoke;
+int MQ135_analog = A1;
+int lpg, co, smoke, air_quality;
 
 MQ2 mq2(MQ2_analog);
 
@@ -17,6 +23,9 @@ DHT dht(dhtpin,DHT11);
 
 void setup() {
 
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), count, FALLING);
+
   dht.begin();
   Serial.begin(9600); 
   
@@ -24,6 +33,12 @@ void setup() {
 }
 
 void loop() {
+
+  if (millis() - millisBefore > 1000) {
+    rpm = (holes / 12.0)*60;
+    holes = 0;
+    millisBefore = millis();
+  }
 
   temp = dht.readTemperature();
   humid = dht.readHumidity();
@@ -35,9 +50,21 @@ void loop() {
   co = mq2.readCO();
   smoke = mq2.readSmoke();
 
+  air_quality = analogRead(MQ135_analog);
+
   Serial.println(lpg);
   Serial.println(co);
   Serial.println(smoke);
 
+  Serial.println(air_quality);
+
+  Serial.println(rpm);
+
+
 }
+
+void count() {
+  holes++;
+}
+
 
