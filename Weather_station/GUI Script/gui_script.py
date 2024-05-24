3101d5f3ef6aa16f8254dba59f85fad9ff2242ec
Adham-Eid27 <adhame900@gmail.com>
2024-05-24 15:56:36 +0300

    gui test

--- Weather_station/GUI Script/gui_script.py
+++ Weather_station/GUI Script/gui_script.py
@@ -13,6 +13,16 @@ serialCom.setDTR(True)
 # reading values
 index = []
 
+# window
+window = Tk()
+window.title("Digital design project")
+window.geometry("1920x1080")
+
+# grid
+window.columnconfigure(0,weight=1)
+window.columnconfigure(1,weight=1)
+window.rowconfigure(0,weight=1)
+window.rowconfigure(1,weight=1)
 
 def readserial():
     s_bytes = serialCom.readline()
@@ -21,19 +31,23 @@ def readserial():
 
     index.append(decoded_bytes)
     if len(index) == 1:  # Temperature
-        Label(window, text=index[0]).place(x=0, y=0)
+        Label(window, text=index[0])
     elif len(index) == 2:  # Humidity
-        Label(window, text=index[1]).place(x=0, y=0)
+        Label(window, text=index[1])
     elif len(index) == 3:  # LPG
-        Label(window, text=index[2]).place(x=0, y=0)
+        Label(window, text=index[2])
     elif len(index) == 4:  # Air quality
-        Label(window, text=index[3]).place(x=500, y=250)
+        Label(window, text=index[3]).place(x=200, y=0)
     elif len(index) == 5:  # RPM
-        Label(window, text=index[4]).place(x=500, y=300)
-    elif len(index) == 6:  # HUmid
-        Label(window, text=index[5]).place(x=500, y=350)
+        text = index[4]
+        rpm = float(text)
+        kmh = 0.0082938*rpm
+        kmh=int(kmh)
+        Label(window, text=kmh).place(x=500, y=200)
+    elif len(index) == 6:  # Humid
+        Label(window, text=index[5]).place(x=200, y=220)
     elif len(index) == 7:  # Temp
-        Label(window, text=index[6]).place(x=500, y=400)
+        Label(window, text=index[6]).place(x=200, y=200)
 
     if len(index) == 7:
         print("")
@@ -42,19 +56,8 @@ def readserial():
     time.sleep(0.0001)
 
 
-window = Tk()
-window.title("Digital design project")
-window.geometry("1920x1080")
 
-Label(window, text="Temperature:").place(x=150, y=10)
-Label(window, text="Humidity:").place(x=150, y=60)
-Label(window, text="Moisture:").place(x=150, y=110)
-Label(window, text="FAN ON/OFF").place(x=400, y=60)
-Label(window, text="PUMP ON/OFF").place(x=400, y=110)
 
-fr = Frame(window).place(x=150, y=10)
-
-lbl = Label(window, text=("Temperature:"))
 
 while True:
     window.update()
